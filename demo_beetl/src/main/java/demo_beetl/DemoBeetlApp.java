9aee49c38a9992a4eac1b32c259f3454c4654a3a
noear <noear@live.cn>
2021-06-12 20:03:41 +0800

    1.5.0

--- demo_beetl/src/main/java/demo_beetl/DemoBeetlApp.java
+++ demo_beetl/src/main/java/demo_beetl/DemoBeetlApp.java
@@ -5,8 +5,9 @@ import org.noear.solon.Solon;
 /**
  * @author noear 2021/6/12 created
  */
-public class App {
+public class DemoBeetlApp {
     public static void main(String[] args) {
-        Solon.start(App.class, args);
+        Solon.start(DemoBeetlApp.class, args)
+                .onError(e -> e.printStackTrace());
     }
 }
---
+++ demo_enjoy/src/main/java/demo_enjoy/dso/AuthProcessorImpl.java
@@ -0,0 +1,31 @@
+package demo_enjoy.dso;
+
+import org.noear.solon.auth.AuthProcessorBase;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author noear 2021/6/12 created
+ */
+public  class AuthProcessorImpl extends AuthProcessorBase {
+    @Override
+    protected List<String> getPermissions() {
+        List<String> list = new ArrayList<>();
+
+        list.add("user:add");
+        list.add("user:demo");
+
+        return list;
+    }
+
+    @Override
+    protected List<String> getRoles() {
+        List<String> list = new ArrayList<>();
+
+        list.add("admin1");
+        list.add("admin2");
+
+        return list;
+    }
+}
