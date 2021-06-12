a8529b48114fac75fd1e72a5400cea9c25cc7b05
noear <noear@live.cn>
2021-06-12 21:27:30 +0800

    1.5.0

---
+++ demo_velocity/src/main/java/demo_velocity/controller/DemoController.java
@@ -0,0 +1,20 @@
+package demo_velocity.controller;
+
+import org.noear.solon.annotation.Controller;
+import org.noear.solon.annotation.Mapping;
+import org.noear.solon.core.handle.ModelAndView;
+
+/**
+ * @author noear 2021/6/12 created
+ */
+@Controller
+public class DemoController {
+    @Mapping("/")
+    public Object test(){
+        ModelAndView model = new ModelAndView("velocity.vm");
+        model.put("title","dock");
+        model.put("message","你好 world!");
+
+        return model;
+    }
+}
