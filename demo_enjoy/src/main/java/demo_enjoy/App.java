dc10b280254c7cd714044ff4e9c111c0c4717827
noear <noear@live.cn>
2021-06-12 19:44:27 +0800

    1.5.0

---
+++ demo_enjoy/src/main/java/demo_enjoy/App.java
@@ -0,0 +1,12 @@
+package demo_enjoy;
+
+import org.noear.solon.Solon;
+
+/**
+ * @author noear 2021/6/12 created
+ */
+public class App {
+    public static void main(String[] args) {
+        Solon.start(App.class, args);
+    }
+}
---
+++ demo_freemarker/src/main/java/demo_freemarker/App.java
@@ -0,0 +1,12 @@
+package demo_freemarker;
+
+import org.noear.solon.Solon;
+
+/**
+ * @author noear 2021/6/12 created
+ */
+public class App {
+    public static void main(String[] args) {
+        Solon.start(App.class, args);
+    }
+}
