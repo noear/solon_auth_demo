0ba57a1c17bfc9387d75bf4e197eb0dd37de4908
noear <noear@live.cn>
2022-07-30 22:48:54 +0800

    1.9.2

--- demo_enjoy/src/main/java/demo_enjoy/dso/AuthProcessorImpl.java
+++ demo_enjoy/src/main/java/demo_enjoy/dso/AuthProcessorImpl.java
@@ -9,6 +9,12 @@ import java.util.List;
  * @author noear 2021/6/12 created
  */
 public  class AuthProcessorImpl extends AuthProcessorBase {
+
+    @Override
+    public boolean verifyLogined() {
+        return true;
+    }
+
     @Override
     protected List<String> getPermissions() {
         List<String> list = new ArrayList<>();
